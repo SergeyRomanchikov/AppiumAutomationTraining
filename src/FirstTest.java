c696057db9e6b917c960104bd71f36fd4b0e6e5e
Sergey Romanchikov <sergey.romanchikov@yahoo.com>
2018-07-29 12:47:48 +0300

    Ex7 Orientation
    Default orientation capability added

--- src/FirstTest.java
+++ src/FirstTest.java
@@ -31,6 +31,7 @@ public class FirstTest {
         capabilities.setCapability("appPackage", "org.wikipedia");
         capabilities.setCapability("appActivity", ".main.MainActivity");
         capabilities.setCapability("app", "/Users/sergeyromanchikov/Documents/GitHub/AppiumAutomationTraining/apks/org.wikipedia.apk");
+        capabilities.setCapability("orientation", "PORTRAIT");
 
         driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
     }
